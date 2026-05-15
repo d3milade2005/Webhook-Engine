d29c471a25f8ec748935881c49f5b757434b557f
Olakunle Damilade <145236066+d3milade2005@users.noreply.github.com>
2026-05-15 21:58:26 +0100

    initialize Spring Boot application with Maven setup and basic configurations

---
+++ src/test/java/com/webhookengine/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webhookengine.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
